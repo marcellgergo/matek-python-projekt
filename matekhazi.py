f9615439d57ea3ae91b4008b8c326be35938b7c4
marcellgergo <71643467+marcellgergo@users.noreply.github.com>
2024-11-18 15:53:06 +0100

    Add files via upload

--- matekhazi.py
+++ matekhazi.py
@@ -1,5 +1,4 @@
 import math
-
 pi = math.pi
 lista = ["kocka", "téglatest", "henger", "gúla", "kúp", "gömb", "tetraéder"]
 def alakzat_kerdes():         
@@ -11,17 +10,36 @@ def alakzat_kerdes():
             break
     return valasz
 
+
 test = alakzat_kerdes()
+if test == 'kocka':
+    def kocka_oldal(felszin, terfogat):
+        return felszin / 6
+    def kocka_terfogat(oldal):
+        return oldal ** 3
+    def kocka_felszin(oldal):
+        return oldal ** 2 * 6
+  
+    print('A kocka térfogata:', kocka_terfogat(15), 'cm3')
+    print('A kocka felszíne:', kocka_felszin(15), 'cm2')
 
-def kocka_oldal(felszin, terfogat):
-    return felszin / 6
-def kocka_terfogat(oldal):
-    return oldal ** 3
 
-def kocka_felszin(oldal):
-    return oldal ** 2 * 6
-        
-print(kocka_terfogat(15), kocka_felszin(15))
+test = alakzat_kerdes()
+if test == 'téglatest':
+    def teglatest_oldal1(felszin, terfogat, oldal2, oldal3):
+        return terfogat / oldal2 / oldal3
+    def teglatest_oldal2(felszin, terfogat, oldal1, oldal3):
+        return terfogat / oldal1 / oldal3
+    def teglatest_oldal3(felszin, terfogat, oldal1, oldal2):
+        return terfogat / oldal1 / oldal2
+
+    def teglatest_terfogat(oldal1, oldal2, oldal3):
+        return oldal1 * oldal2 * oldal3
+    def teglatest_felszin(oldal1, oldal2, oldal3):
+        return 2 * (oldal1*oldal2 + oldal2*oldal3 + oldal1*oldal3)
+
+    print('A téglatest térfogata:', teglatest_terfogat(), 'cm3')
+    print('A téglatest felszíne:', teglatest_felszin(), 'cm2')
 
 
 """def readFloat(s):
@@ -32,10 +50,8 @@ print(kocka_terfogat(15), kocka_felszin(15))
                 return value
             except ValueError:
                 print ("Please enter the price with 2 decimal places")"""
-
 """r = readFloat("Add meg az alapkör sugarát: ") #alapkör sugara
 M = readFloat("Add meg a kúp magasságát: ") #kúp magassága
 a = input("Add meg az alkotó hosszát: ") #alkotó hossza
 A = input("Add meg a kúp felszínét: ") #felszín
 V = input("Add meg a kúp térfogatát: ") #térfogat"""
-
